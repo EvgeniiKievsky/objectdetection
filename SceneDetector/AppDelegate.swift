a256091851875fe362407f9b7f0f2437cc3c94bf
Evgenii Kievsky <jkievsky@kapowai.online>
2017-10-26 00:31:20 +0300

    initital commit

---
+++ SceneDetector/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SceneDetector
+//
+//  Created by Kievsky Evgenii on 10/18/17.
+//  Copyright © 2017. All rights reserved.
+//
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
